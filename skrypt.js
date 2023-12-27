8b442b1535688aab7365124ae27d70e71a64db88
s26964-pj <115402943+s26964-pj@users.noreply.github.com>
2023-12-27 19:18:41 +0100

    update

--- skrypt.js
+++ skrypt.js
@@ -27,8 +27,36 @@ function getWeather() {
     });
 }
 
+function displayDailyForecast(dailyForecast) {
+    // Pobieramy sekcję, gdzie chcemy wyświetlić prognozę
+    const weatherSection = $('#weather-section');
+
+    // Iterujemy przez dni prognozy i wypełniamy kontenery
+    for (const day in dailyForecast) {
+        if (dailyForecast.hasOwnProperty(day)) {
+            const dayData = dailyForecast[day];
+            const containerId = `day${day}`;
+
+            // Tworzymy elementy HTML
+            const container = $(`<div id="${containerId}" class="day-forecast"></div>`);
+            const date = $(`<p id="date-${day}">${dayData.date.toLocaleDateString()}</p>`);
+            const temperature = $(`<p id="temperature-${day}">${dayData.temperature}°C</p>`);
+            const description = $(`<p id="description-${day}">${dayData.description}</p>`);
+
+            // Dodajemy elementy do kontenera
+            container.append(date, temperature, description);
+
+            // Dodajemy kontener do sekcji prognozy
+            weatherSection.append(container);
+        }
+    }
+}
+
 function displayWeatherForecast(data) {
     // 'data.list' zawiera prognozę pogody na 5 dni z interwałem co 3 godziny
+
+    // Oczyść kontenery przed dodaniem nowych danych
+    $('#weather-section').empty();
 
     // Obliczamy dzień dla pierwszego wpisu
     const firstDate = new Date(data.list[0].dt * 1000); // Timestamp w sekundach
@@ -44,17 +72,13 @@ function displayWeatherForecast(data) {
 
         // Sprawdzamy, czy już istnieje prognoza dla tego dnia
         if (!dailyForecast[day]) {
-                dailyForecast[day] = {
+            dailyForecast[day] = {
                 date: date,
                 temperature: item.main.temp,
                 description: item.weather[0].description
             };
         }
     });
-    displayDailyForecast(dailyForecast);
-    // Wyświetlamy prognozy na stronie
-}
 
-function displayDailyForecast(dailyForecast) {
-    
+    displayDailyForecast(dailyForecast);
 }
