af2516024f9ccae7ed2d82ae0ac0fd651bab96eb
s26964-pj <115402943+s26964-pj@users.noreply.github.com>
2024-01-12 21:26:58 +0100

    done

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const apiKey = '5f82816ae5f6e914aea705bf5d99f1d7';
 
 // Funkcja do pobierania prognozy pogody na 5 dni
 function getWeather() {
-    // Pobieramy nazwę miasta z pola tekstowego
+    // Pobieranie nazwę miasta z pola tekstowego
     const cityName = $('#city').val();
 
-    // Sprawdzamy, czy użytkownik wprowadził nazwę miasta
+    // Sprawdzanie, czy użytkownik wprowadził nazwę miasta
     if (!cityName) {
         alert('Please enter a city name');
         return;
@@ -13,22 +13,22 @@ function getWeather() {
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
 
-    // Wysyłamy zapytanie AJAX do API OpenWeatherMap
+    // Wysyłanie zapytania AJAX do API OpenWeatherMap
     $.ajax({
         url: apiUrl,
         method: 'GET',
         success: function (data) {
-            // Sprawdzamy, czy uzyskano poprawne dane prognozy
+            // Sprawdzanie, czy uzyskano poprawne dane prognozy
             if (data && data.list && data.list.length > 0) {
-                // Wywołujemy funkcję do wyświetlania prognozy
+                // Wywołanie funkcji do wyświetlania prognozy
                 displayWeatherForecast(data);
             } else {
-                // Wyświetlamy alert w przypadku błędu pobierania danych
+                // Wyświetlanie alertu w przypadku błędu pobierania danych
                 alert('Error retrieving weather data. Please try again.');
             }
         },
         error: function (error) {
-            // Logujemy błąd w konsoli oraz wyświetlamy alert o błędzie
+            // Logowanie błędu w konsoli oraz wyświetlanie alertu o błędzie
             console.log('Error:', error);
             alert('Error retrieving weather data. Please try again.');
         }
@@ -36,18 +36,18 @@ function getWeather() {
 }
 
 function displayWeatherForecast(data) {
-    // Oczyść kontenery przed dodaniem nowych danych
+    // Czyszczenie kontenerów przed dodaniem nowych danych
     $('#weather-section').empty();
 
     // Obiekt, który będzie przechowywał prognozę dla każdego dnia
     const dailyForecast = {};
 
-    // Przetwarzamy dane prognozy
+    // Przetwarzanie danych prognozy
     data.list.forEach(item => {
         const date = new Date(item.dt * 1000);
         const day = date.getDate();
 
-        // Sprawdzamy, czy już istnieje prognoza dla tego dnia
+        // Sprawdzanie, czy już istnieje prognoza dla tego dnia
         if (!dailyForecast[day]) {
             dailyForecast[day] = {
                 date: date,
@@ -63,17 +63,17 @@ function displayWeatherForecast(data) {
             };
         }
 
-        // Określamy, czy to dzień czy noc
+        // Określanie, czy to dzień czy noc
         const isDay = date.getHours() >= 6 && date.getHours() < 18;
 
-        // Dodajemy temperaturę do odpowiedniej tablicy
+        // Dodawanie temperatury do odpowiedniej tablicy
         if (isDay) {
             dailyForecast[day].temperatures.day.push(item.main.temp);
         } else {
             dailyForecast[day].temperatures.night.push(item.main.temp);
         }
 
-        // Dodajemy prognozę godzinową
+        // Prognoza godzinowa
         dailyForecast[day].hourlyForecast.push({
             hour: date.getHours(),
             temperature: item.main.temp,
@@ -86,36 +86,36 @@ function displayWeatherForecast(data) {
         return Math.round(degrees * 2) / 2;
     }
 
-    // Pobieramy sekcję, gdzie chcemy wyświetlić prognozę
+    // Pobieranie sekcji, gdzie wyświetlamy prognozę
     const weatherSection = $('#weather-section');
 
-    // Sortujemy daty przed wyświetleniem
+    // Sortowanie daty przed wyświetleniem
     const sortedDays = Object.keys(dailyForecast).sort((a, b) => dailyForecast[a].date - dailyForecast[b].date);
 
-    // Iterujemy przez dni prognozy i wypełniamy kontenery
+    // Iterowanie przez dni prognozy i wypełnianie kontenerów
     sortedDays.forEach(day => {
         const dayData = dailyForecast[day];
         const containerId = `day${day}`;
 
-        // Tworzymy elementy HTML
+        // Tworzenie elementu HTML
         const container = $(`<div id="${containerId}" class="day-forecast"></div>`);
         const date = $(`<h3 id="date-${day}">${dayData.date.toLocaleDateString()}</h3>`);
         const description = $(`<p id="description-${day}">${dayData.description}</p>`);
         const icon = $(`<img id="weather-icon-${day}" src="https://openweathermap.org/img/wn/${dayData.icon}.png" alt="Weather Icon">`);
 
-        // Liczymy średnią temperaturę dla dnia i nocy
+        // Liczenie średniej temperatury dla dnia i nocy
         const avgTemperatureDay = dayData.temperatures.day.length > 0 ? dayData.temperatures.day.reduce((a, b) => a + b, 0) / dayData.temperatures.day.length : null;
         const avgTemperatureNight = dayData.temperatures.night.length > 0 ? dayData.temperatures.night.reduce((a, b) => a + b, 0) / dayData.temperatures.night.length : null;
 
-        // Tworzymy elementy HTML dla temperatury
+        // Tworzenie elementu HTML dla temperatury
         const temperatureDay = avgTemperatureDay !== null ? $(`<p id="temperature-day-${day}">Day Temperature: ${roundToHalfDegrees(avgTemperatureDay)}°C</p>`) : null;
         const temperatureNight = avgTemperatureNight !== null ? $(`<p id="temperature-night-${day}">Night Temperature: ${roundToHalfDegrees(avgTemperatureNight)}°C</p>`) : null;
 
-        // Tworzymy elementy HTML dla prognozy godzinowej
+        // Tworzenie elementu HTML dla prognozy godzinowej
         const hourlyForecastContainer = $(`<div id="hourly-forecast-${day}" class="hourly-forecast"></div>`);
         const hourlyForecastTitle = $('<h4>Hourly Forecast</h4>');
 
-        // Iterujemy przez prognozę godzinową i tworzymy elementy HTML
+        // Iterowanie przez prognozę godzinową i tworzenie elementu HTML
         dayData.hourlyForecast.forEach(hourlyData => {
             const hourlyItem = $(`
                 <div class="hourly-item">
@@ -130,17 +130,14 @@ function displayWeatherForecast(data) {
 
         // Nasłuchiwanie na kliknięcie lub dotknięcie kontenera
         container.on('click tap', function (event) {
-            // Zapobiegaj domyślnej akcji, aby uniknąć ewentualnych problemów
             event.preventDefault();
-
-            // Pokazujemy lub ukrywamy prognozę godzinową
             hourlyForecastContainer.slideToggle();
         });
 
-        // Dodajemy elementy do kontenera
+        // Dodanie elementów do kontenera
         container.append(date, description, icon, temperatureDay, temperatureNight, hourlyForecastTitle, hourlyForecastContainer);
 
-        // Dodajemy kontener do sekcji prognozy
+        // Dodanie kontenera do sekcji prognozy
         weatherSection.append(container);
     });
 }
@@ -150,7 +147,7 @@ function refreshData() {
     getWeather();
 }
 
-// Obsługa zdarzenia kliknięcia przycisku
+// Obsługa zdarzenia kliknięcia przycisku "Search"
 $(document).ready(function () {
     $('#search-btn').on('click', function () {
         getWeather();
@@ -160,5 +157,5 @@ $(document).ready(function () {
     $('#welcome-message').addClass('fadeIn');
 
     // Odświeżanie danych
-    setInterval(refreshData, 60000); // 60000 milisekund = 1 minuta
+    setInterval(refreshData, 60000);
 });
